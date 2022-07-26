d6990c1922d773e4826cd3ba10a5317efd7f1b6c
Sovember <michaelajohnsonresume@gmail.com>
2022-07-26 02:02:53 -0500

    FIRST

---
+++ src/main/java/com/tutorial/springJPAtutorial/SpringJpAtutorialApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.springJPAtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpAtutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpAtutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/springJPAtutorial/SpringJpAtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.springJPAtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpAtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
